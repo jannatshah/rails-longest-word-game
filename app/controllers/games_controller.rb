866126573193c1a1430c92c43ad1e2c50c10da55
Jannat Shah <jannat.shah@gmail.com>
2019-11-11 15:52:21 +0000

    Longest word game, working with no style. Have not yet done optional pieces of the exercise

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(9) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @answer = params[:answer]
+    @grid = params[:grid]
+
+    scoring_method(@answer, @grid)
+  end
+
+  def in_the_grid?(attempt, grid)
+    letters = attempt.upcase.chars
+    letters.all? { |letter| letters.count(letter) <= grid.count(letter) }
+  end
+
+  def attempt_valid?(attempt)
+    attempt_url = open("https://wagon-dictionary.herokuapp.com/#{attempt}")
+    attempt_hash = JSON.parse(attempt_url.read)
+    attempt_hash['found']
+  end
+
+  def scoring_method(answer, grid)
+    @result = { score: 0, message: '' }
+    if attempt_valid?(answer) == false
+      @result[:message] = "Your answer #{answer.upcase} is not an English word"
+    elsif in_the_grid?(answer, grid) == false
+      @result[:message] = "Your answer #{answer.upcase} is not in the grid"
+    else
+      @result[:score] = answer.length
+      @result[:message] = "Well done. #{answer.upcase} is a great answer!"
+    end
+
+    @result
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'new', to: 'games#new', as: :new
+  get 'score', to: 'games#score', as: :score
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'score', to: 'games#score'
 end
