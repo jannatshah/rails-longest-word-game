2aff6b86985245c770c78fa56cc0719ec1f4612a
Jannat Shah <jannat.shah@gmail.com>
2019-11-11 16:55:31 +0000

    Updated for a few optional exercise attemptes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 
 class GamesController < ApplicationController
   def new
-    @letters = Array.new(9) { ('A'..'Z').to_a.sample }
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
